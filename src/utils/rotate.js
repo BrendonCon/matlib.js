9ecebbb0a2e0cf5e68ba9863f146946f4249765d
Brendon <brendoncon@gmail.com>
2020-03-25 17:22:14 +0200

    Fix equation

--- src/utils/rotate.js
+++ src/utils/rotate.js
@@ -2,6 +2,6 @@ export default (point, theta) => {
   let c = Math.cos(theta);
   let s = Math.sin(theta);
   let { x, y } = point; 
-  point.x = x * c + y * s;
+  point.x = x * c - y * s;
   point.y = x * s + y * c;
 };
